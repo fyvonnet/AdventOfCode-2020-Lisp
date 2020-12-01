d8e21cb17c6df99446fc44b967c289fe81e4cb6d
Franck YVONNET <franck@yvonnet.fr>
2020-12-01 07:50:19 +0100

    reduced all recursive functions to only one

--- day01.lisp
+++ day01.lisp
@@ -4,37 +4,17 @@
 
 (in-package :day01)
 
-(defun find-part1 (elm1 lst)
-  (labels ((rec (elm2 lst)
-                (unless (null elm2)
-                  (if (= 2020 (+ elm1 elm2))
-                    (* elm1 elm2)
-                    (rec (first lst) (rest lst))))))
-    (rec (first lst) (rest lst))))
 
-(defun find-part2-sub (elm1 elm2 lst)
-  (labels ((rec (elm3 lst)
-                (unless (null elm3)
-                  (if (= 2020 (+ elm1 elm2 elm3))
-                    (* elm1 elm2 elm3)
-                    (rec (first lst) (rest lst))))))
-    (rec (first lst) (rest lst))))
-
-(defun find-part2 (elm1 lst)
-  (unless (null lst)
-    (let ((res (find-part2-sub elm1 (first lst) (rest lst))))
-      (if (null res)
-        (find-part2 elm1 (rest lst))
-        res))))
-
-(defun find-solution (lst func)
-  (let ((res (funcall func (first lst) (rest lst))))
-    (if (null res)
-      (find-solution (rest lst) func)
-      res)))
+(defun find-solution (entries needed &optional candidates)
+  (if (zerop needed)
+    (when (= 2020 (reduce #'+ candidates))
+      (reduce #'* candidates))
+    (when entries
+      (let
+        ((result (find-solution (rest entries) (1- needed) (cons (first entries) candidates))))
+        (if result result (find-solution (rest entries) needed candidates))))))
 
 (defun main ()
   (let
-    ((input (read-input-as-list 1 #'parse-integer)))
-    (format t "~a~%" (find-solution input #'find-part1))
-    (format t "~a~%" (find-solution input #'find-part2))))
+    ((entries (read-input-as-list 1 #'parse-integer)))
+    (dolist (n '(2 3)) (print (find-solution entries n)))))
