6c1f60c5c71053333af4cf8e28660e3671cdd06d
Franck YVONNET <franck@yvonnet.fr>
2020-12-01 08:00:25 +0100

    use trivia

--- day01.lisp
+++ day01.lisp
@@ -1,20 +1,20 @@
 (defpackage :day01
-  (:use :cl :aoc-misc)
+  (:use :cl :aoc-misc :trivia)
   (:export main))
 
 (in-package :day01)
-
 
 (defun find-solution (entries needed &optional candidates)
   (if (zerop needed)
     (when (= 2020 (reduce #'+ candidates))
       (reduce #'* candidates))
     (when entries
-      (let
-        ((result (find-solution (rest entries) (1- needed) (cons (first entries) candidates))))
-        (if result result (find-solution (rest entries) needed candidates))))))
+        (match (find-solution (rest entries) (1- needed) (cons (first entries) candidates))
+          (nil (find-solution (rest entries) needed candidates))
+          (solution solution)))))
 
 (defun main ()
   (let
     ((entries (read-input-as-list 1 #'parse-integer)))
     (dolist (n '(2 3)) (print (find-solution entries n)))))
+
