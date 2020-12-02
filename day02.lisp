8b5bac73f96a7647b267428ca7d884af26e53b1a
Franck YVONNET <franck@yvonnet.fr>
2020-12-02 09:00:32 +0100

    part 2

--- day02.lisp
+++ day02.lisp
@@ -17,21 +17,24 @@
 (defun count-valid (predicate lst)
   (if (null lst)
     0
-    (let ((ret (count-valid predicate (rest lst))))
-      (if (funcall predicate (first lst)) (1+ ret) ret))))
+    (+
+      (if (funcall predicate (first lst)) 1 0)
+      (count-valid predicate (rest lst)))))
 
-(defun valid-record (record)
-  (let*
-    ((password (coerce (getf record :password) 'list))
-     (valid-letters
+(defun policy-1 (record)
+  (let
+    ((valid-letters
        (count-valid
          (lambda (c) (char= (getf record :letter) c))
-         password)))
+         (coerce (getf record :password) 'list))))
     (and (>= valid-letters (getf record :low)) (<= valid-letters (getf record :high)))))
 
-
-
+(defun policy-2 (record)
+  (= 1 (count-valid
+         (lambda (i) (char= (getf record :letter) (aref (getf record :password) (1- i))))
+         (list (getf record :low) (getf record :high)))))
 
 (defun main ()
   (let ((input (read-input-as-list 2 #'parse-line)))
-    (print (count-valid #'valid-record input))))
+    (dolist (policy (list #'policy-1 #'policy-2))
+      (format t "~a~%" (count-valid policy input)))))
