8eac9142ff119b1f18f6a8860472dcdc48ea61d8
Franck YVONNET <franck@yvonnet.fr>
2020-12-05 09:40:53 +0100

    part 2

--- day05.lisp
+++ day05.lisp
@@ -5,18 +5,31 @@
 (in-package :day05)
 
 
+(defun decode-char (c)
+  (case c
+    (#\F 0) (#\B 1) (#\L 0) (#\R 1)
+    (otherwise (error (format nil "Unexpected character: ~a~%" c)))))
+
 (defun seat-id (str)
   (labels
     ((rec (lst bitvalue)
           (if (null lst)
             0
-            (+
-              (if (or (char= (first lst) #\F) (char= (first lst) #\L))
-                0
-                bitvalue)
-              (rec (rest lst) (* 2 bitvalue))))))
+            (+ (* (decode-char (first lst)) bitvalue)
+               (rec (rest lst) (* 2 bitvalue))))))
     (rec (reverse (coerce str 'list)) 1)))
 
+(defun search-seat (lst)
+  (let*
+    ((fst (first lst))
+     (snd (second lst)))
+    (unless (null snd)
+      (let ((ret (search-seat (rest lst))))
+        (if (= snd (+ 2 fst)) (cons (1+ fst) ret) ret)))))
 
 (defun main ()
-  (print (reduce #'max (read-input-as-list 5 #'seat-id))))
+  (let
+    ((seat-ids (read-input-as-list 5 #'seat-id)))
+    (print (reduce #'max seat-ids))
+    (print (search-seat (sort seat-ids #'<)))))
+
