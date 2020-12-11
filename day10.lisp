e981eda34a58ce0be9848cd6ce519e6065d1ecf1
Franck YVONNET <franck@yvonnet.fr>
2020-12-11 16:29:37 +0100

    rewrite without graph

--- day10.lisp
+++ day10.lisp
@@ -2,7 +2,6 @@
   (:use :cl :aoc-misc :functional-queue :trivia)
   (:export main)
   (:import-from :fset
-                :convert
                 :empty-map
                 :lookup
                 :with
@@ -11,45 +10,31 @@
 (in-package :day10)
 
 
-(defun make-graph (lst)
-  (if (null lst)
-    (empty-map)
-    (with
-      (make-graph (cdr lst))
-      (car lst)
-      (remove-if (lambda (r) (> (- r (car lst)) 3)) (cdr lst)) )))
+(defun count-diffs (lst &optional (counts (list 0 0)))
+  (match lst
+         ((list _) (reduce #'* counts))
+         ((cons a (cons b _))
+          (count-diffs
+            (cdr lst)
+            (case (- b a)
+              (1 (list (1+ (car counts)) (cadr counts)))
+              (3 (list (car counts) (1+ (cadr counts))))
+              (otherwise counts))))))
 
-(defun chain-adapters (graph end-node)
-  (let ((count1 0) (count3 0))
-    (labels
-      ((rec (node)
-            (if (= node end-node)
-              (* count1 count3)
-              (let ((next-node (car (lookup graph node))))
-                (case (- next-node node) (1 (incf count1)) (3 (incf count3)))
-                (rec next-node)))))
-      (rec 0))))
-
-(defun count-combinations (graph)
-  (labels
-    ((rec (lst map)
-          (match (car lst)
-                 ((cons node nil) (lookup map node))
-                 ((cons node neighbs)
-                  (rec
-                    (cdr lst)
-                    (let ((node-value (lookup map node)))
-                      (reduce
-                        (lambda (m n) (with m n (+ (lookup map n) node-value)))
-                        neighbs :initial-value map)))))))
-    (rec (convert 'list graph) (with (with-default (empty-map) 0) 0 1))))
+(defun count-combinations (adapters &optional (map (with (with-default (empty-map) 0) 0 1)))
+  (match adapters
+         ((list last-adapter) (lookup map last-adapter))
+         ((cons curr-adapter next-adapters)
+          (count-combinations
+            next-adapters
+            (reduce
+              (lambda (m a) (with m a (+ (lookup map curr-adapter) (lookup m a))))
+              (take-until (lambda (a) (> (- a curr-adapter) 3)) next-adapters)
+              :initial-value map)))))
 
 (defun main ()
   (let*
-    ((input (read-input-as-list 10 #'parse-integer))
-     (built-in (+ 3 (reduce #'max input)))
-     (adapters (cons built-in input))
-     (graph (make-graph (sort (cons 0 adapters) #'<))))
-    (print (chain-adapters graph built-in))
-    (print (count-combinations graph))))
-
+    ((input (sort (read-input-as-list 10 #'parse-integer) #'<))
+     (adapters (cons 0 (snoc input (+ 3 (car (last input)))))))
+    (print (count-diffs adapters))
+    (print (count-combinations adapters))))
