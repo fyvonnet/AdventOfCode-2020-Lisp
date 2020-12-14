6a7462969d123238cf0867905e7f5f2d1abd1b58
Franck YVONNET <franck@yvonnet.fr>
2020-12-14 08:38:03 +0100

    part 2

--- day14.lisp
+++ day14.lisp
@@ -30,13 +30,64 @@
         (#\X val))
       (* 2 n))))
 
+(defun apply-bitmask (mask addr)
+  (if (null mask)
+    nil
+    (multiple-value-bind (q r) (floor addr 2)
+      (cons
+        (if (char= #\0 (car mask))
+          (if (zerop r) #\0 #\1)
+          (car mask))
+        (apply-bitmask (cdr mask) q)))))
+
+(defun convert-to-int (addr &optional (n 1))
+  (if (null addr)
+    0
+    (case (car addr)
+      (#\0 (convert-to-int (cdr addr) (* 2 n)))
+      (#\1 (+ n (convert-to-int (cdr addr) (* 2 n)))))))
+
+(defun make-address (base-addr n)
+    (when base-addr
+      (if (char= #\X (car base-addr))
+        (multiple-value-bind (q r) (floor n 2)
+          (cons (if (zerop r) #\0 #\1) (make-address (cdr base-addr) q)))
+        (cons (car base-addr) (make-address (cdr base-addr) n)))))
+
+(defun make-all-addresses (base-addr count)
+  (unless (zerop count)
+    (cons
+      (convert-to-int (make-address base-addr (1- count)))
+      (make-all-addresses base-addr (1- count)))))
+
+(defun all-addresses (mask addr)
+  (let* 
+    ((base-addr (apply-bitmask mask addr))
+     (addr-count (expt 2 (count-valid (lambda (c) (char= c #\X)) base-addr))))
+    (make-all-addresses base-addr addr-count)))
+
 (defun run-program (program &optional mask (memory (empty-map)))
   (match (car program)
          (nil (convert 'list memory))
          ((list 'mask m) (run-program (cdr program) m memory))
          ((list 'mem addr value) (run-program (cdr program ) mask (with memory addr (apply-mask mask value))))))
 
+(defun run-program2 (program &optional mask (memory (empty-map)))
+  (match (car program)
+         (nil (convert 'list memory))
+         ((list 'mask m) (run-program2 (cdr program) m memory))
+         ((list 'mem addr value)
+          (run-program2
+            (cdr program)
+            mask
+            (reduce
+              (lambda (m a) (with m a value))
+              (all-addresses mask addr)
+              :initial-value memory)))))
+
 (defun main ()
   (let
     ((input (read-input-as-list 14 #'decode-line)))
-    (print (reduce #'+ (mapcar #'cdr (run-program input))))))
+    (print (reduce #'+ (mapcar #'cdr (run-program input))))
+    (print (reduce #'+ (mapcar #'cdr (run-program2 input))))))
+
