226bce06597c3aca648213371e159e789efcdce0
Franck YVONNET <franck@yvonnet.fr>
2020-12-15 11:05:18 +0100

    personal coding of the Chinese remainder theorem

--- day13.lisp
+++ day13.lisp
@@ -5,6 +5,33 @@
 (in-package :day13)
 
 
+; https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+(defun euclidian (a b)
+  (labels
+    ((rec (ra rb sa sb ta tb)
+          (multiple-value-bind (q r) (floor ra rb)
+            (let
+              ((new-s (- sa (* q sb)))
+               (new-t (- ta (* q tb))))
+              (if (zerop r)
+                (cons sb tb)
+                (rec rb r sb new-s tb new-t))))))
+    (rec a b 1 0 0 1)))
+
+; https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+(defun crt (as ns)
+  (let*
+    ((great-N  (reduce #'* ns))
+     (great-Ns (mapcar (lambda (n) (/ great-N n)) ns)))
+    (mod
+      (reduce
+        #'+
+        (mapcar
+          (lambda (great-N n a) (* (car (euclidian great-N n)) a great-N))
+          great-Ns ns as))
+      great-N)))
+
+
 (defun get-ids (str)
   (labels
     ((rec (ids offset)
@@ -14,39 +41,6 @@
                    (n (cons (cons n offset) (rec (cdr ids) (1+ offset))))))))
     (rec (split "," str) 0)))
 
-;;
-;; Calculates the GCD of a and b based on the Extended Euclidean Algorithm. The function also returns
-;; the Bézout coefficients s and t, such that gcd(a, b) = as + bt.
-;;
-;; The algorithm is described on page http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Iterative_method_2
-;;
-(defun egcd (a b)
-  (do ((r (cons b a) (cons (- (cdr r) (* (car r) q)) (car r))) ; (r+1 r) i.e. the latest is first.
-       (s (cons 0 1) (cons (- (cdr s) (* (car s) q)) (car s))) ; (s+1 s)
-       (u (cons 1 0) (cons (- (cdr u) (* (car u) q)) (car u))) ; (t+1 t)
-       (q nil))
-    ((zerop (car r)) (values (cdr r) (cdr s) (cdr u)))       ; exit when r+1 = 0 and return r s t
-    (setq q (floor (/ (cdr r) (car r))))))                     ; inside loop; calculate the q
-
-;;
-;; Calculates the inverse module for a = 1 (mod m). 
-;;
-;; Note: The inverse is only defined when a and m are coprimes, i.e. gcd(a, m) = 1.”
-;;
-(defun invmod (a m)
-  (multiple-value-bind (r s k) (egcd a m)
-    (unless (= 1 r) (error "invmod: Values ~a and ~a are not coprimes." a m))  
-    s))
-
-(defun chinese-remainder (am)
-  "Calculates the Chinese Remainder for the given set of integer modulo pairs.
-  Note: All the ni and the N must be coprimes."
-  (loop :for (a . m) :in am
-        :with mtot = (reduce #'* (mapcar #'(lambda(X) (cdr X)) am))
-        :with sum  = 0
-        :finally (return (mod sum mtot))
-        :do
-        (incf sum (* a (invmod (/ mtot m) m) (/ mtot m)))))
 
 (defun main ()
   (let*
@@ -64,5 +58,5 @@
           (mapcar #'first bus-ids)))
       ((cons line departure) (print (* line (- departure earliest-timestamp)))))
 
-    (print (chinese-remainder (mapcar (lambda (i o) (cons (- i o) i)) ids offsets)))))
+    (print (crt (mapcar (lambda (i o) (- i o)) ids offsets) ids))))
 
