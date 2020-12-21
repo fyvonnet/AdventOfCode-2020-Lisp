fce84b8b98c8a60e2af8990d3b8dc73dfad5a5c6
Franck YVONNET <franck@yvonnet.fr>
2020-12-21 17:35:33 +0100

    part 2

--- day21.lisp
+++ day21.lisp
@@ -2,7 +2,7 @@
   (:use :cl :aoc-misc :cl-ppcre :trivia)
   (:export main)
   (:import-from :fset :contains? :convert :lookup
-                      :empty-map :empty-set :with))
+                :empty-map :empty-set :set-size :with))
 
 (in-package :day21)
 
@@ -13,13 +13,35 @@
       (split " " (aref matches 0))
       (split ", " (aref matches 1)))))
 
-
 (defun add-to-map (map line)
   (match line 
          ((cons ingredients allergens)
           (reduce
-            (lambda (m a) (with m a (cons (list-to-set ingredients) (lookup m a))))
+            (lambda (m a) (with m a (cons (convert 'fset:set ingredients) (lookup m a))))
             allergens :initial-value map))))
+
+(defun has-one-ingredient (allergen)
+  (= 1 (set-size (cdr allergen))))
+
+(defun confirm-ingredients (lst &optional confirmed-pairs)
+  (if (null lst)
+    confirmed-pairs
+    (let* 
+      ((confirmed-ingredients (remove-if-not #'has-one-ingredient lst))
+       (unconfirmed-ingredients (remove-if #'has-one-ingredient lst))
+       (ingredients-set (reduce #'fset:union (mapcar #'cdr confirmed-ingredients))))
+       (confirm-ingredients
+         (mapcar 
+           (lambda (i)
+             (match i
+                    ((cons name ingredients)
+                     (cons
+                       name
+                       (fset:set-difference ingredients ingredients-set)))))
+           unconfirmed-ingredients)
+         (reduce 
+           (lambda (l i) (cons (cons (car i) (first (convert 'list (cdr i)))) l))
+           confirmed-ingredients :initial-value confirmed-pairs)))))
 
 (defun main ()
   (let*
@@ -29,13 +51,29 @@
        (reduce
          #'add-to-map input
          :initial-value (empty-map)))
+     (allergens-list (convert 'list allergens-map))
+     (intersected-allergens
+       (mapcar
+         #'cons
+         (mapcar #'car allergens-list)
+         (mapcar 
+           (lambda (i) (reduce #'fset:intersection i))
+           (mapcar #'cdr allergens-list))))
      (nonallergenic-ingredients-set
        (fset:set-difference
-         (list-to-set all-ingredients-list)
+         (convert 'fset:set all-ingredients-list)
          (reduce
            #'fset:union
-           (mapcar
-             (lambda (i) (reduce #'fset:intersection i))
-             (mapcar 'cdr (convert 'list allergens-map)))))))
-    (print (count-valid (lambda (i) (contains? nonallergenic-ingredients-set i)) all-ingredients-list))
-    (print allergens-map)))
+           (mapcar 'cdr (convert 'list intersected-allergens)))))
+     (ordered-allergenic-ingredients
+       (mapcar 
+         #'cdr 
+         (sort 
+           (confirm-ingredients intersected-allergens)
+           (lambda (a b) (string< (car a) (car b)))))))
+
+    (format t "~a~%" (count-valid (lambda (i) (contains? nonallergenic-ingredients-set i)) all-ingredients-list))
+    (format t "~a" (car ordered-allergenic-ingredients))
+    (dolist (i (cdr ordered-allergenic-ingredients)) (format t ",~a" i))
+    (format t "~%")))
+
