f16d38b39ddc24475d09e3052b67dc997765bf5e
Franck YVONNET <franck@yvonnet.fr>
2020-12-25 09:02:00 +0100

    part 2

--- day24.lisp
+++ day24.lisp
@@ -1,7 +1,7 @@
 (defpackage :day24
   (:use :cl :aoc-misc :trivia)
   (:export main)
-  (:import-from :fset :empty-set :contains? :less :size :with)
+  (:import-from :fset :empty-set :contains? :convert :less :size :with)
   (:import-from :serapeum :nlet))
 
 (in-package :day24)
@@ -22,13 +22,43 @@
                (#\e (rec     x (1- y) (1+ z) (cddr lst)))
                (#\w (rec (1- x)    y  (1+ z) (cddr lst)))))))))
 
-(defun main ()
-  (let
-    ((input (read-input-as-list 24 #'find-turned-tile)))
-    (print
-      (size
-        (reduce
-          (lambda (s c) (if (contains? s c) (less s c) (with s c)))
-          input
-          :initial-value (empty-set))))))
+(defun coords-range (n)
+  (loop
+    with results
+    for x from (- n) to n
+    do (loop
+         for y from (max (- n) (- (- x) n)) to (min n (+ (- x) n))
+         for coord = (list x y (- (- x) y))
+         do (setf results (cons coord results)))
+    finally (return results)))
 
+(defparameter *surroundings* (remove-if (lambda (c) (equal '(0 0 0) c)) (coords-range 1)))
+
+(defun count-surrounding-black-tiles (floor coord)
+  (count-valid
+    (lambda (c) (contains? floor c))
+    (mapcar (lambda (s) (mapcar #'+ s coord)) *surroundings*)))
+
+(defun update-floor (n floor)
+  (if (zerop n)
+    floor
+    (update-floor
+      (1- n)
+      (reduce
+        (lambda (s c)
+          (let ((n (count-surrounding-black-tiles floor c)))
+            (if (contains? floor c)
+              (if (or (zerop n) (> n 2)) s (with s c))
+              (if (= 2 n) (with s c) s))))
+        (coords-range (1+ (reduce (lambda (a b) (max (abs a) (abs b))) (reduce #'append (convert 'list floor)))))
+        :initial-value (empty-set)))))
+
+(defun main ()
+  (let*
+    ((black-tiles
+       (reduce
+         (lambda (s c) (if (contains? s c) (less s c) (with s c)))
+         (read-input-as-list 24 #'find-turned-tile)
+         :initial-value (empty-set))))
+    (print (size black-tiles))
+    (print (size (update-floor 100 black-tiles)))))
