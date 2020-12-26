1d261288fc2b7eeedb3c2a41b9f8c9227c3dca67
Franck YVONNET <franck@yvonnet.fr>
2020-12-26 12:41:52 +0100

    part 2

--- day17.lisp
+++ day17.lisp
@@ -12,71 +12,71 @@
 (in-package :day17)
 
 
-(defvar *surroundings*
-  (list
-    '(-1 -1 -1) '(0 -1 -1) '(1 -1 -1)
-    '(-1  0 -1) '(0  0 -1) '(1  0 -1)
-    '(-1  1 -1) '(0  1 -1) '(1  1 -1)
+(defvar *surroundings*)
 
-    '(-1 -1  0) '(0 -1  0) '(1 -1  0)
-    '(-1  0  0)            '(1  0  0)
-    '(-1  1  0) '(0  1  0) '(1  1  0)
+(defun modulos (n divs)
+  (when divs
+    (multiple-value-bind (q r) (floor n (car divs))
+      (cons r (modulos q (cdr divs))))))
 
-    '(-1 -1  1) '(0 -1  1) '(1 -1  1)
-    '(-1  0  1) '(0  0  1) '(1  0  1)
-    '(-1  1  1) '(0  1  1) '(1  1  1)))
+(defun all-coords (dims &optional (shift 0))
+  (nlet rec ((n (reduce #'* dims)) (coords nil))
+        (if (zerop n)
+          coords
+          (rec (1- n)
+               (cons
+                 (mapcar
+                   (lambda (x) (+ shift x))
+                   (modulos (1- n) dims))
+                 coords)))))
 
-
-(defun make-initial-set (arr)
-  (nlet rec ((y 0) (x 0) (set (empty-set)))
-        (cond
-          ((= x (array-dimension arr 0))
-           (rec (1+ y) 0 set))
-          ((= y (array-dimension arr 1)) set)
-          (t
-            (rec y (1+ x)
-                 (case (aref arr y x)
-                   (#\# (with set (list x y 0)))
-                   (#\. set)))))))
-
-(defun update-space (space new-min-coord new-dim-space)
-  (let*
-    ((new-max-coord (mapcar (lambda (d) (+ new-min-coord d)) new-dim-space))
-     (new-space (empty-set)))
-    (loop
-      for z from new-min-coord below (third new-max-coord)
-      do (loop
-           for y from new-min-coord below (second new-max-coord)
-           do (loop
-                for x from new-min-coord below (first new-max-coord)
-                do (let*
-                     ((coord (list x y z))
-                      (active-around
-                        (count-valid
-                          (lambda (c) (contains? space c))
-                          (mapcar (lambda (s) (mapcar #'+ s coord)) *surroundings*))))
-                     (if (contains? space coord)
-                       (when (or (= 2 active-around) (= 3 active-around)) (setf new-space (with new-space coord)))
-                       (when (= 3 active-around) (setf new-space (with new-space coord))))))))
-    new-space))
-
-(defun update-space-times (n space min-coord dim-space)
+(defun update-space (n space min-coord space-dims)
   (if (zerop n)
     (length (convert 'list space))
     (let*
-      ((new-min-coord (1- min-coord))
-       (new-dim-space (mapcar (lambda (d) (+ 2 d)) dim-space)))
-      (update-space-times
+      ((new-min-coord  (1- min-coord))
+       (new-space-dims (mapcar (lambda (d) (+ 2 d)) space-dims)))
+      (update-space
         (1- n)
-        (update-space space new-min-coord new-dim-space)
-        new-min-coord
-        new-dim-space))))
+        (reduce
+          (lambda (new-space coord)
+            (let*
+              ((active-around
+                 (count-if
+                   (lambda (c) (contains? space c))
+                   (mapcar (lambda (s) (mapcar #'+ s coord)) *surroundings*))))
+              (if (contains? space coord)
+                (if (or (= 2 active-around) (= 3 active-around)) (with new-space coord) new-space)
+                (if (= 3 active-around) (with new-space coord) new-space))))
+          (all-coords new-space-dims new-min-coord)
+          :initial-value (empty-set))
+        new-min-coord new-space-dims))))
+
+(defun run-space (input dim)
+  (let*
+    ( (height (array-dimension input 0))
+     (width  (array-dimension input 1))
+     (dims (append (list height width) (replicate (- dim 2) 1)))
+     (initial-set 
+       (reduce 
+         (lambda (set coord)
+           (match coord 
+                  ((cons x (cons y _))
+                   (case (aref input y x)
+                     (#\# (with set coord))
+                     (#\. set)))))
+         (all-coords dims)
+         :initial-value (empty-set))))
+    (setf *surroundings*
+          (remove-if
+            (lambda (c) (every #'zerop c))
+            (all-coords (replicate dim 3) -1)
+            :count 1))
+    (update-space 6 initial-set 0 dims)))
 
 (defun main ()
   (let*
-    ((input (read-input-as-array 17))
-     (initial-set (make-initial-set input))
-     (height (array-dimension input 0))
-     (width  (array-dimension input 1)))
-    (print (update-space-times 6 initial-set 0 (list width height 1)))))
+    ((input (read-input-as-array 17)))
+    (dolist (dim '(3 4))
+      (format t "~a~%" (run-space input dim)))))
 
