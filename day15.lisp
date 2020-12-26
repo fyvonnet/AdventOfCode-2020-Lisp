4331e6c743444b59fcb2340e26a41d87b6712d5a
Franck YVONNET <franck@yvonnet.fr>
2020-12-26 15:37:38 +0100

    use vector instead of map

--- day15.lisp
+++ day15.lisp
@@ -1,30 +1,9 @@
 (defpackage :day15
-  (:use :cl :aoc-misc :cl-ppcre :trivia)
-  (:export main)
-  (:import-from :fset
-                :empty-seq
-                :lookup
-                :with))
+  (:use :cl :aoc-misc :cl-ppcre)
+  (:export main))
 
 (in-package :day15)
 
-
-(defun initialize-seq (numbers &optional (turn 1) (seq (empty-seq)))
-  (if (null numbers)
-    seq
-    (initialize-seq
-      (cdr numbers)
-      (1+ turn)
-      (with seq (car numbers) turn))))
-
-(defun play-game (seq number turn last-turn)
-  (if (= last-turn turn)
-    number
-    (play-game
-      (with seq number turn)
-      (match (lookup seq number) (nil 0) (last-seen (- turn last-seen)))
-      (1+ turn)
-      last-turn)))
 
 (defun main ()
   (let*
@@ -32,7 +11,23 @@
        (mapcar 
          #'parse-integer
          (split "," (first (read-input-as-list 15)))))
-     (seq (initialize-seq (butlast starting-numbers))))
-    (dolist (last-turn '(2020 30000000))
-      (print (play-game seq (first (last starting-numbers)) (length starting-numbers) last-turn)))))
+     (seen  (make-array 30000000 :initial-element nil))
+     (turns (make-array 30000000))
+     (start-turn
+       (loop 
+         for i from 0
+         for n in (butlast starting-numbers)
+         do (setf (aref seen n) i)
+         finally (return i))))
 
+    (loop
+      with last = (first (last starting-numbers))
+      for i from start-turn below 30000000
+      do (setf (aref turns i) last)
+      do (let
+           ((found (aref seen last)))
+           (setf (aref seen last) i)
+           (setf last (if (null found) 0 (- i found)))))
+
+    (dolist (i '(2020 30000000))
+      (print (aref turns (1- i))))))
